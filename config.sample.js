8566f422ac4fb2669eb3fd58edb0f59bee91c60d
aknts <25027425+aknts@users.noreply.github.com>
2021-05-07 23:33:42 +0300

    Update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -1,6 +1,2 @@
-/*
-Μain config object that should be delivered through node-red
-{"globalsettings":{"broker":"mqtt://192.168.2.240:1883","logtopic":"global/logz"},"nextnode":"b3e5aac371d7e2","mynodeid":"b3e5aac371d7e1","previousnode":"b3e5aac371d7e0","appname":"mqttFilter","appsettings":{"rate_transmit":"1000","rate_sampling":"60","txpower":18.5,"plex":2}}
-*/
 var config = 'eyJnbG9iYWxzZXR0aW5ncyI6eyJicm9rZXIiOiJtcXR0Oi8vMTkyLjE2OC4yLjI0MToxODgzIiwibG9ndG9waWMiOiJnbG9iYWwvbG9neiIsIm5hbWVpZCI6InN0cmFuZCIsIm51bW9mYXBwbW9kdWxlcyI6IjUifSwibmV4dG5vZGUiOiJiM2U1YWFjMzcxZDcwNSIsIm15bm9kZWlkIjoiYjNlNWFhYzM3MWQ3MDQiLCJwcmV2aW91c25vZGUiOiJiM2U1YWFjMzcxZDcwMyIsImFwcG5hbWUiOiJtcXR0RmlsdGVyIiwiYXBwc2V0dGluZ3MiOnsicmF0ZV90cmFuc21pdCI6IjEwMDAiLCJyYXRlX3NhbXBsaW5nIjoiNjAiLCJyYXRlX3JlY29ubmVjdCI6MTAwMDAsInR4cG93ZXIiOjE4LjUsInBsZXgiOjJ9fQ';
 module.exports = config;
